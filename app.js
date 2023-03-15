270f8598ce4830b3350397e19e9e6dae386adc70
cheatchansovoth <cheatchansovoth@gmail.com>
2023-03-15 13:51:40 +1300

    change port

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express =require('express');
 const app=express();
+require('dotenv').config()
 
 app.use(express.json());
 
@@ -8,7 +9,7 @@ app.get('/',(req,res)=>
     console.log('Hello world');
 })
 
-app.listen(5000,(req,res)=>
+app.listen(process.env.PORT ||8080,(req,res)=>
 {
     console.log('Hello world');
 })
